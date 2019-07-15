23335a6cd72e3510877c4006cf0a03c79dc80f1e
Graham Fairweather <xotic750@gmail.com>
2019-07-15 12:25:40 +0200

    :bookmark: v2.0.9
    Auto build

--- dist/is-nan-x.js
+++ dist/is-nan-x.js
@@ -2,13 +2,13 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2017-present",
-  "date": "2019-07-14T21:30:25.684Z",
+  "date": "2019-07-15T10:25:36.193Z",
   "describe": "",
   "description": "ES6-compliant shim for Number.isNaN - the global isNaN returns false positives.",
   "file": "is-nan-x.js",
   "hash": "a3200608ec6628009b5c",
   "license": "MIT",
-  "version": "2.0.8"
+  "version": "2.0.9"
 }
 */
 (function webpackUniversalModuleDefinition(root, factory) {

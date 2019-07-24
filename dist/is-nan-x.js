30e08be3c608183ce4b90dc62cf1ac910a0ebb23
Graham Fairweather <xotic750@gmail.com>
2019-07-24 15:10:35 +0200

    :bookmark: v2.0.23
    Add jsdelivr

--- dist/is-nan-x.js
+++ dist/is-nan-x.js
@@ -2,13 +2,13 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2017-present",
-  "date": "2019-07-23T23:30:31.362Z",
+  "date": "2019-07-24T13:10:29.761Z",
   "describe": "",
   "description": "ES6-compliant shim for Number.isNaN - the global isNaN returns false positives.",
   "file": "is-nan-x.js",
   "hash": "bd51c37a153af57104ff",
   "license": "MIT",
-  "version": "2.0.22"
+  "version": "2.0.23"
 }
 */
 (function webpackUniversalModuleDefinition(root, factory) {

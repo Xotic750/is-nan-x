62d5f51577735aac625de7bbcc7779976e48280a
Graham Fairweather <xotic750@gmail.com>
2019-08-04 21:02:36 +0200

    Auto run

--- dist/is-nan-x.js
+++ dist/is-nan-x.js
@@ -2,7 +2,7 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2017-present",
-  "date": "2019-08-04T13:49:38.046Z",
+  "date": "2019-08-04T19:02:31.259Z",
   "describe": "",
   "description": "ES6-compliant shim for Number.isNaN - the global isNaN returns false positives.",
   "file": "is-nan-x.js",
